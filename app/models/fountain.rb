eaef0af88b62df3f47a2f5ef6a283e6db80117de
Maegan Burke <maegan.burke@gmail.com>
2015-08-05 19:56:50 +0200

    fixed syntax error in fountain.rb

--- app/models/fountain.rb
+++ app/models/fountain.rb
@@ -1,7 +1,8 @@
 class Fountain < ActiveRecord::Base
 
-  has_many :fountain_comments :fountain_ratings :fountain_checkins
-
+  has_many :fountain_comments
+  has_many :fountain_ratings
+  has_many :fountain_checkins
   validates :name, presence: true, uniqueness: true
   validates :location, presence: true
 
