f68e4eba5a9bc084cf6124cbc410a3bd899c21af
Maegan Burke <maegan.burke@gmail.com>
2015-08-05 20:57:11 +0200

    updated admin controller

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -1,12 +1,27 @@
 class AdminController < ApplicationController
 
   before_filter :authorized?
-  private
+
+  def create
+    @admin = Admin.new(admin_params)
+    if @admin.valid?
+      @admin.save
+      session[:admin_id] = @admin.id
+    else
+      render :new
+    end
+  end
+
+private
   def authorized?
     unless current_user.has_role? :admin
       flash[:error] = "You are not authorized to view that page."
       redirect_to root_path
     end
   end
-  
+
+  def admin_params
+    params.require(:admin).permit(:username, :email, :password, :password_confirmation)
+  end
+
 end
