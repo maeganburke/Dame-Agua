b1bc27aef168a704a92ee0e67bb5ee615ef5a869
Maegan Burke <maegan.burke@gmail.com>
2015-08-06 21:14:57 +0200

    updated

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -8,7 +8,7 @@ class SessionsController < ApplicationController
     if user && user.authenticate(params[:password])
       session[:user_id] = user.id
       flash[:notice] = "Get excited! You've successfully signed in."
-      redirect_to dameagua_path
+      redirect_to '/dameagua#dameagua'
     else
       flash[:notice] = "Oopsies! Something went wrong. You sure that's your password?"
       redirect_to '/'
